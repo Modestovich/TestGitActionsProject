63e900c8443e90c414226294a4e58c98640742e3
Stanislav <modestovich9@gmail.com>
2025-09-22 14:59:43 +0300

    Update MainActivity.kt

--- app/src/main/java/com/org/testgitactionsproject/MainActivity.kt
+++ app/src/main/java/com/org/testgitactionsproject/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.org.testgitactionsproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : Activity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
 }
